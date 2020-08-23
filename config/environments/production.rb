a98d238ea9accf7718b4ac9a53b6aafa2e574c70
shivareddy <shiva.r547@gmail.com>
2020-08-23 18:02:59 +0530

    commit files for assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,4 +110,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.active_storage.service = :cloudinary
+
+  config.assets.paths << Rails.root.join('assets', 'fonts')
+  config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
 end
