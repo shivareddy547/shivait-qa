e85d9a3603ff83cbf3434c5a410842f112d1b68c
shivareddy <shiva.r547@gmail.com>
2020-09-09 08:24:55 +0530

    spree editor

--- spree/spree_editor/app/models/ckeditor/picture.rb
+++ spree/spree_editor/app/models/ckeditor/picture.rb
@@ -4,8 +4,8 @@ class Ckeditor::Picture < Ckeditor::Asset
   # Ckeditor::Picture.attachment_definitions[:data][:path] = '/:class/:id/:style/:basename.:extension'
   # Ckeditor::Picture.attachment_definitions[:data][:url] = '/:class/:id/:style/:basename.:extension'
 
-  validates_attachment_size :data, less_than: 2.megabytes
-  validates_attachment_presence :data
+  # validates_attachment_size :data, less_than: 2.megabytes
+  # validates_attachment_presence :data
 
   def url_content
     url(:content)
